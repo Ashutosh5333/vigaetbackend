435f63f0a66dd8f5d87983c140c867b6111ee74e
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-03-29 10:48:29 +0530

    feat: done

--- config/db.connect.js
+++ config/db.connect.js
@@ -7,8 +7,6 @@ require("dotenv").config();
       await mongoose.connect(process.env.mongourl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000,
-        socketTimeoutMS: 45000,
       });
       console.log(`MongoDB connected with server ${mongoose.connection.host}`);
     } catch (error) {
