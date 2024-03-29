9e5a428143daba63addd9b1190392ebac9856963
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-03-29 10:58:20 +0530

    feat: done

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const Coonectdatabase = require('./config/db.connect')
  const port = process.env.PORT || 8080;
 
  process.on("uncaughtException", (err) => {
-   console.log(`Error: ${err.message}`);
-   console.log(`Shutting down the server due to uncaught exception`);
-   process.exit(1);
- });
- 
- const server = app.listen(port,async () =>{
-    await Coonectdatabase();
-    console.log(`Server Runing on port ${port}`)
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
+});
 
- })
+const server = app.listen(port,async () =>{
+   await Coonectdatabase();
+   console.log(`Server Runing on port ${port}`)
 
- 
- process.on("unhandledRejection", (err) => {
-   console.log(`Error: ${err.message}`);
-   console.log(`Shutting down the server due to Unhabled promise rejection`);
-   server.close(() => {
-     process.exit(1);
-   });
- });
+})
+
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Shutting down the server due to Unhabled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
